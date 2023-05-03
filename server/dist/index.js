386b15bb54b5d73a8e6c11dcb052d22672b365b5
Zakwan Ashfaq Zian <zakwanashfaq@gmail.com>
2023-05-03 16:14:37 -0230

    server setup

---
+++ server/dist/index.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import express from 'express';
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+app.get('/', (req, res) => {
+    res.send('Server initialized');
+});
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
